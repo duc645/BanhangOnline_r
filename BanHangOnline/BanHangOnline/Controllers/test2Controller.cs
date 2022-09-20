300af47f02391106aaba3493adb1c3bdf1966e37
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-09-20 13:49:05 +0700

    test2_commit

---
+++ BanHangOnline/BanHangOnline/Controllers/test2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanHangOnline.Controllers
+{
+    public class test2Controller : Controller
+    {
+        // GET: test2
+        public ActionResult Index()
+        {
+            //test2
+            return View();
+        }
+    }
+}
