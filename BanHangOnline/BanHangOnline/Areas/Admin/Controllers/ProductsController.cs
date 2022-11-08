471cd04ca394295106b0626ce005849d4a53c850
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-11-08 23:20:32 +0700

    form hiển thị danh mục sản phẩm , form thêm sản phẩm (đa thêm và hiển thị được), form thêm mới sản phẩm(nhiều ảnh)

---
+++ BanHangOnline/BanHangOnline/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using BanHangOnline.Models;
+using BanHangOnline.Models.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanHangOnline.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Admin/Products
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+
+        public ActionResult Index(string searchText, int? page)
+        {
+            var pageSize = 10;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<Product> items = _dbContext.Products.OrderByDescending(x => x.Id);
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                items = items.Where(x => x.Alias.Contains(searchText) || x.Title.Contains(searchText));
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            ViewBag.SearchText = searchText;
+            return View(items);
+        }
+        public ActionResult Add()
+        {
+            ViewBag.ProductCategory = new SelectList(_dbContext.ProductCategories.ToList(), "Id", "Title");
+            return View();
+        }
+    }
+}
--- BanHangOnline/BanHangOnline/Models/EF/ProductCategory.cs
+++ BanHangOnline/BanHangOnline/Models/EF/ProductCategory.cs
@@ -21,17 +21,20 @@ namespace BanHangOnline.Models.EF
 
         [Required(ErrorMessage = "Trường này không được để trống")]
         [StringLength(150)]
+       
         public string Title { get; set; }
+        [Required]
+        [StringLength(150)]
         public string Alias { get; set; }
-
+        [StringLength(150)]
         public string Icon { get; set; }
-
+        [StringLength(250)]
         public string Description { get; set; }
-
+        [StringLength(250)]
         public string SeoTitle { get; set; }
-
+        [StringLength(500)]
         public string SeoDescription { get; set; }
-
+        [StringLength(250)]
         public string SeoKeywords { get; set; }
 
         //public ICollection<Product_Category> Product_Categories { get; set; }
