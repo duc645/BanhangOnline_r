b9ee98274aad93b5bb605b97b58a4b099cb68f2c
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-11-27 20:46:04 +0700

    T13-commit 2 - Thêm js phần chỉnh sửa số lượng trang chi tiết, thêm phần cho sản phẩm vào giỏ, xóa sách ở giỏ , chỉnh sửa số lượng + số tiền ( chưa ok lắm) , tạo view phần thanh toán

---
+++ BanHangOnline/BanHangOnline/Controllers/ShoppingCartController.cs
@@ -0,0 +1,110 @@
+﻿using BanHangOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanHangOnline.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult CheckOut()
+        {
+
+            return View();
+        }
+
+        public ActionResult Partial_Item_Cart()
+        {
+            ShoppingCart cart = (ShoppingCart)Session["Cart"];
+            if (cart != null)
+            {
+                return PartialView(cart.items);
+            }
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart(int id,int quantity)
+        {
+            var code = new { Success = false, msg = "", code = -1, Count = 0 };
+            var db = new ApplicationDbContext();
+            var checkProduct = db.Products.Where(x => x.Id == id).Include(c=>c.ProductCategory).FirstOrDefault();
+            if(checkProduct != null)
+            {
+                ShoppingCart cart = (ShoppingCart)Session["Cart"];
+                if(cart == null)
+                {
+                    cart = new ShoppingCart();
+                }
+                ShoppingCartItem item = new ShoppingCartItem
+                {
+                    ProductId = checkProduct.Id,
+                    ProductName = checkProduct.Title,
+                    CategoryName = checkProduct.ProductCategory.Title,
+                    ProductImg = checkProduct.Image,
+                    Quantity = quantity
+                };
+                item.Price = checkProduct.Price;
+                if (checkProduct.PriceSale > 0)
+                {
+                    item.Price = (decimal)checkProduct.PriceSale;
+                }
+                item.TotalPrice = item.Quantity * item.Price;
+                cart.AddToCart(item, quantity);
+                code = new { Success = true, msg = " Đã thêm sản phẩm vào giỏ hàng", code = 1, Count = cart.items.Count };
+                Session["Cart"] = cart;
+            }
+            return Json(code);
+
+        }
+        public ActionResult ShowCount()
+        {
+            ShoppingCart cart = (ShoppingCart)Session["Cart"];
+            if (cart != null)
+            {
+                return Json(new { Count = cart.items.Count }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { Count = 0 }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var code = new { Success = false, msg = "", code = -1, Count = 0 };
+
+            ShoppingCart cart = (ShoppingCart)Session["Cart"];
+            if (cart != null)
+            {
+                var checkProduct = cart.items.FirstOrDefault(x => x.ProductId == id);
+                if (checkProduct != null)
+                {
+                    cart.Remove(id);
+                    code = new { Success = true, msg = "", code = 1, Count = cart.items.Count };
+                }
+            }
+            return Json(code);
+        }
+
+        [HttpPost]
+        public ActionResult Update(int id, int quantity)
+        {
+            ShoppingCart cart = (ShoppingCart)Session["Cart"];
+            if (cart != null)
+            {
+                cart.UpdateQuantity(id, quantity);
+                return Json(new { Success = true });
+            }
+            return Json(new { Success = false });
+        }
+    }
+}
--- BanHangOnline/BanHangOnline/Scripts/jsShopping.js
+++ BanHangOnline/BanHangOnline/Scripts/jsShopping.js
@@ -1,4 +1,7 @@
-﻿$(document).ready(function () {
+﻿
+$(document).ready(function () {
+    ShowCount();
+    initQuantity();
     $('body').on('click', '.btnAddToCart', function (e) {
         e.preventDefault();
         var id = $(this).data('id');
@@ -7,6 +10,147 @@
         if (tQuantity != '') {
             quantity = parseInt(tQuantity);
         }
-        alert(id + '     ' + quantity);
+/*        alert(id + '     ' + quantity);*/
+
+        $.ajax({
+            url: '/shoppingcart/addtocart',
+            type: 'POST',
+            data: { id: id, quantity: quantity },
+            success: function (rs) {
+                if (rs.Success) {
+                    $('#cart_cout').html(rs.Count);
+                    alert(rs.msg);
+                }
+            }
+        });
     });
+
+    $('body').on('click', '.btnDelete', function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        var conf = confirm('Bạn có chắc muốn xóa sản phẩm này khỏi giỏ hàng?');
+        if (conf == true) {
+            $.ajax({
+                url: '/shoppingcart/Delete',
+                type: 'POST',
+                data: { id: id },
+                success: function (rs) {
+                    if (rs.Success) {
+                        $('#cart_cout').html(rs.Count);
+                        $('#trow_' + id).remove();
+                        LoadCart();
+                    }
+                }
+            });
+        }
+
+    });
+    //$('.txtQuantity').off('keyup').on('keyup', function () {
+
+    //    var quantity = parseInt($(this).val());
+    //    var productid = parseInt($(this).data('id'));
+    //    var price = parseFloat($(this).data('price'));
+    //    if (isNaN(quantity) == false) {
+
+    //        var amount = quantity * price;
+    //        $('#amout_' + productid).text(amount +' Đ');
+    //    }
+    //    else {
+    //        $('#amout_' + productid).text(0);
+    //    }
+    //            $('#lblTotalOrder').text(GetTotalOrder() + ' Đ');
+    //    Update(productid, quantity);
+    //});
+    var timeout = null;
+    $('body').off('keyup').on('keyup', '.txtQuantity', function (e) {
+        clearTimeout(timeout);
+        e.preventDefault();
+        var quantity = parseInt($(this).val());
+        var productid = parseInt($(this).data('id'));
+        var price = parseFloat($(this).data('price'));
+        timeout = setTimeout(function () {
+            console.log('sau 10 giay');
+
+            Update(productid, quantity);
+        }, 1000);
+
+    });
+
+    //$('body').on('keyup', '.txtQuantity', function (e) {
+    //    e.preventDefault()
+    //    setTimeout(function () {
+    //        console.log('sau 2 giay');
+    //        alert("sau 5 giay");
+    //    }, 2000);
+    //});
 });
+
+//function GetTotalOrder() {
+//    var listTextBox = $('.txtQuantity');
+//    var total = 0;
+//    $.each(listTextBox, function (i, item) {
+//        total += parseInt($(item).val()) * parseFloat($(item).data('price'));
+//    });
+//    return total;
+//}
+
+function ShowCount() {
+    $.ajax({
+        url: '/shoppingcart/ShowCount',
+        type: 'GET',
+        success: function (rs) {
+            $('#cart_cout').html(rs.Count);
+        }
+    });
+}
+
+function Update(id, quantity) {
+    $.ajax({
+        url: '/shoppingcart/Update',
+        type: 'POST',
+        data: { id: id, quantity: quantity },
+        success: function (rs) {
+            if (rs.Success) {
+                LoadCart();
+            }
+        }
+    });
+
+
+    //alert('update');
+    //LoadCart();
+}
+function LoadCart() {
+    $.ajax({
+        url: '/shoppingcart/Partial_Item_Cart',
+        type: 'GET',
+        success: function (rs) {
+            $('#load_data').html(rs);
+        }
+    });
+
+
+/*    alert('loadcart');*/
+}
+
+
+//sua so luong trang chi tiet
+function initQuantity() {
+    if ($('.plus').length && $('.minus').length) {
+        var plus = $('.plus');
+        var minus = $('.minus');
+        var value = $('#quantity_value');
+
+        plus.on('click', function () {
+            var x = parseInt(value.text());
+            value.text(x + 1);
+        });
+
+        minus.on('click', function () {
+            var x = parseInt(value.text());
+            if (x > 1) {
+                value.text(x - 1);
+            }
+        });
+    }
+}
