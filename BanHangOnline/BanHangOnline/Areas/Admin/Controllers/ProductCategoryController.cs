f5462b9226fdc02e2d38b4271978b290a525c23f
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-12-07 01:06:17 +0700

    T25-commit1- thêm phần sửa,xóa, xóa nhiều bản ghi của productCategory bên admin

--- BanHangOnline/BanHangOnline/Areas/Admin/Controllers/ProductCategoryController.cs
+++ BanHangOnline/BanHangOnline/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using BanHangOnline.Models;
 using BanHangOnline.Models.EF;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +13,20 @@ namespace BanHangOnline.Areas.Admin.Controllers
     {
         // GET: Admin/ProductCategory
         private ApplicationDbContext _dbContext = new ApplicationDbContext();
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            var items = _dbContext.ProductCategories.ToList();
+            var pageSize = 10;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<ProductCategory> items = _dbContext.ProductCategories.OrderByDescending(x => x.Id);
+
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+
             return View(items);
         }
         public ActionResult Add()
@@ -39,5 +51,64 @@ namespace BanHangOnline.Areas.Admin.Controllers
             }
             return View(model);
         }
+
+        public ActionResult Edit(int id)
+        {
+            var item = _dbContext.ProductCategories.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProductCategory model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                model.ModifiedDate = DateTime.Now;
+                if(model.Alias == null)
+                {
+                    model.Alias = BanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+
+                }
+                _dbContext.ProductCategories.Attach(model);
+                _dbContext.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = _dbContext.ProductCategories.Find(id);
+            if (item != null)
+            {
+                _dbContext.ProductCategories.Remove(item);
+                _dbContext.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+        [HttpPost]
+        public ActionResult deletedAll(string ids)
+        {
+            if (!string.IsNullOrEmpty(ids))
+            {
+                var items = ids.Split(',');
+                if (items != null && items.Any())
+                {
+                    foreach (var item in items)
+                    {
+                        var obj = _dbContext.ProductCategories.Find(Convert.ToInt32(item));
+                        _dbContext.ProductCategories.Remove(obj);
+                        _dbContext.SaveChanges();
+                    }
+
+                }
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+
     }
 }
