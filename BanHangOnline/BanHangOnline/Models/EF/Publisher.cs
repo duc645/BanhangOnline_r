2fedac5a169cfdf00151725f40799a988776643c
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-12-31 23:14:02 +0700

    T34-commit5-ảnh

---
+++ BanHangOnline/BanHangOnline/Models/EF/Publisher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BanHangOnline.Models.EF
+{
+    [Table("tb_Publisher")]
+    public class Publisher: CommonAbstract
+    {
+        public Publisher()
+        {
+            //this.Product_Categories = new HashSet<Product_Category>();
+            this.Products = new HashSet<Product>();
+        }
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Trường này không được để trống")]
+        [StringLength(150, ErrorMessage = "Không vượt quá 150 ký tự")]
+
+        public string FullName { get; set; }
+        [StringLength(150, ErrorMessage = "Không vượt quá 150 ký tự")]
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
